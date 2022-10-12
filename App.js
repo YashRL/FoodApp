e21f9b680c89f120c121e1f54a24ff22189d7808
YashRL <93367850+YashRL@users.noreply.github.com>
2022-10-12 13:09:12 +0530

    initial commit
    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { createAppContainer } from "react-navigation";
+import {createStackNavigator}  from "react-navigation-stack"
+import SearchScreen from "./src/screens/SearchScreen";
+
+const navigator = createStackNavigator({ 
+  Search : SearchScreen
+
+}, {
+  initialRouteName : 'Search',
+  defaultNavigationOptions : {
+    title : 'Business Search'
+  }
+});
+
+export default createAppContainer(navigator);
